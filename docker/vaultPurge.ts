89f7d6086968bbb6d12ad7e2dafc2b866bb87e98
NorkzYT <richard@pcscorp.dev>
2025-06-29 02:09:33 -0400

    🔧 (docker): update network name in docker-compose and fix formatting in vaultPurge script Refactor the network name for clarity and improve code consistency in the vaultPurge script.

--- docker/vaultPurge.ts
+++ docker/vaultPurge.ts
@@ -1,15 +1,15 @@
-import puppeteer from "puppeteer";
-import * as dotenv from "dotenv";
-import { authenticator } from "otplib";
-import { join } from "path";
-import { writeFileSync } from "fs";
+import puppeteer from 'puppeteer';
+import * as dotenv from 'dotenv';
+import { authenticator } from 'otplib';
+import { join } from 'path';
+import { writeFileSync } from 'fs';
 
 dotenv.config();
 
 (async () => {
   const browser = await puppeteer.launch({
     headless: true,
-    args: ["--no-sandbox"],
+    args: ['--no-sandbox'],
   });
   const page = await browser.newPage();
   await page.setViewport({ width: 1920, height: 1080 });
@@ -21,74 +21,77 @@ dotenv.config();
 
   if (!bitwardenUrl || !email || !password || !otpSecret) {
     console.error(
-      "Host, email, password, or OTP secret environment variables are not set"
+      'Host, email, password, or OTP secret environment variables are not set'
     );
     process.exit(1);
   }
 
   try {
     await page.goto(bitwardenUrl, {
-      waitUntil: "networkidle2",
+      waitUntil: 'networkidle2',
       timeout: 60000,
     });
 
-    console.log("Page loaded.");
+    console.log('Page loaded.');
 
     // Type in email
-    await page.click("input#login_input_email");
+    await page.click('input#login_input_email');
     await page.keyboard.type(email, { delay: 10 });
-    console.log("Email entered.");
+    console.log('Email entered.');
 
     // Click the Continue button
     await page.evaluate(() => {
       const continueButton = Array.from(
-        document.querySelectorAll("button")
-      ).find((button) => button.textContent?.includes("Continue"));
+        document.querySelectorAll('button')
+      ).find((button) => button.textContent?.includes('Continue'));
       if (continueButton) {
         (continueButton as HTMLElement).click();
       }
     });
-    console.log("Clicked Continue button after email.");
+    console.log('Clicked Continue button after email.');
 
     // Type in password
-    await page.click("input#login_input_master-password");
+    await page.click('input#login_input_master-password');
     await page.keyboard.type(password, { delay: 10 });
-    console.log("Password entered.");
+    console.log('Password entered.');
 
     // Click Log In button
-    await page.click('button[type="submit"]');
-    console.log("Clicked Log In button.");
+    await page.keyboard.press('Tab');
+    await page.keyboard.press('Tab');
+    await page.keyboard.press('Tab');
+    await page.keyboard.press('Enter');
+    console.log('Clicked Log In button.');
 
     // Wait for navigation
-    await page.waitForNavigation({ waitUntil: "networkidle2", timeout: 60000 });
-    console.log("Navigation after Log In.");
+    await page.waitForNavigation({ waitUntil: 'networkidle2', timeout: 60000 });
+    console.log('Navigation after Log In.');
 
     // Handle potential 2FA step
-    const twoFactorInputSelector = "input#code";
+    const twoFactorInputSelector = 'input#code';
 
     if (await page.$(twoFactorInputSelector)) {
-      console.log("Two-factor authentication step detected");
+      console.log('Two-factor authentication step detected');
 
       // Generate OTP code
       const otpCode = authenticator.generate(otpSecret);
 
       await page.click(twoFactorInputSelector);
       await page.keyboard.type(otpCode, { delay: 10 });
-      console.log("OTP code entered.");
+      console.log('OTP code entered.');
 
       // Ensure the OTP input field loses focus
-      await page.keyboard.press("Tab");
-      await page.keyboard.press("Tab");
-      await page.keyboard.press("Enter");
-      console.log("Navigation after clicking Continue.");
+      await page.keyboard.press('Tab');
+      await page.keyboard.press('Tab');
+      await page.keyboard.press('Enter');
+      console.log('Navigation after clicking Continue.');
     }
 
-    console.log("Logged in successfully.");
+    console.log('Logged in successfully.');
 
     // Navigate to settings page directly
     const settingsUrl = `${bitwardenUrl}/#/settings`;
-    await page.goto(settingsUrl, { waitUntil: "networkidle2", timeout: 60000 });
-    console.log("Navigated to settings page.");
+    await page.goto(settingsUrl, { waitUntil: 'networkidle2', timeout: 60000 });
+    console.log('Navigated to settings page.');
 
     try {
       const buttonClicked: boolean = await page.evaluate(() => {
@@ -97,7 +100,7 @@ dotenv.config();
             document.querySelectorAll("button[type='button']")
           );
           return buttons.find((button) =>
-            button.textContent?.trim().includes("Purge vault")
+            button.textContent?.trim().includes('Purge vault')
           ) as HTMLButtonElement | undefined;
         };
 
@@ -115,7 +118,7 @@ dotenv.config();
       if (buttonClicked) {
         console.log('Clicked the "Purge vault" button.');
       } else {
-        throw new Error("Purge vault button not found.");
+        throw new Error('Purge vault button not found.');
       }
     } catch (error) {
       console.error(`An error occurred: ${error}`);
@@ -126,15 +129,15 @@ dotenv.config();
 
     // Start typing the password directly
     await page.keyboard.type(password, { delay: 10 });
-    console.log("Typed the master password for confirmation.");
+    console.log('Typed the master password for confirmation.');
 
     // Click the final Purge Vault confirmation button
-    await page.keyboard.press("Tab");
-    await page.keyboard.press("Tab");
-    await page.keyboard.press("Enter");
+    await page.keyboard.press('Tab');
+    await page.keyboard.press('Tab');
+    await page.keyboard.press('Enter');
     await page.evaluate(() => {
       const confirmPurgeButton = document.querySelector(
-        "form .modal-footer .btn.btn-danger.btn-submit"
+        'form .modal-footer .btn.btn-danger.btn-submit'
       );
       if (confirmPurgeButton) {
         (confirmPurgeButton as HTMLElement).click();
@@ -143,24 +146,24 @@ dotenv.config();
     console.log('Clicked the final "Purge vault" confirmation button.');
 
     // Wait for the purge to complete
-    await page.waitForNavigation({ waitUntil: "networkidle2", timeout: 60000 });
-    console.log("Purge completed successfully.");
+    await page.waitForNavigation({ waitUntil: 'networkidle2', timeout: 60000 });
+    console.log('Purge completed successfully.');
   } catch (error) {
-    console.error("An error occurred:", error);
+    console.error('An error occurred:', error);
 
     // Take a screenshot if an error occurs
-    const screenshotPath = join("/bitwardensync/data", "error_screenshot.png");
+    const screenshotPath = join('/bitwardensync/data', 'error_screenshot.png');
     await page.screenshot({ path: screenshotPath });
     console.log(`Screenshot saved at ${screenshotPath}`);
 
     // Save the error message to a file
-    const errorMessagePath = join("/bitwardensync/data", "error_message.txt");
+    const errorMessagePath = join('/bitwardensync/data', 'error_message.txt');
 
     // Check the type of error and handle accordingly
     if (error instanceof Error) {
       writeFileSync(errorMessagePath, error.message);
     } else {
-      writeFileSync(errorMessagePath, "An unknown error occurred.");
+      writeFileSync(errorMessagePath, 'An unknown error occurred.');
     }
 
     console.log(`Error message saved at ${errorMessagePath}`);
